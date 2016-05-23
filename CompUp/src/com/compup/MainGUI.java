507fc8be22436a02c6260eda1bf75f0e3bfad26a
Matt <matthewgable@gmail.com>
2016-05-23 09:24:28 -0400

    Revisions
    --Added BorderLayout structure
    --Reorganized JPanels around BorderLayout
    --Added placeholder buttons and labels

--- CompUp/src/com/compup/MainGUI.java
+++ CompUp/src/com/compup/MainGUI.java
@@ -1,8 +1,9 @@
 package com.compup;
 
-import java.awt.CardLayout;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,11 +14,15 @@ public class MainGUI
 {
 	//Create the default containers
 	private JFrame mainFrame = new JFrame("Main GUI Panel");
-	private JPanel contentPanel = new JPanel();
-	private JPanel topLabelPanel = new JPanel();
-	private JPanel bottomButtonPanel = new JPanel();
+	private JPanel contentPanel = new JPanel(new BorderLayout());
+	private JPanel northPanel = new JPanel();
+	private JPanel westPanel = new JPanel();
+	private JPanel centerPanel = new JPanel();
+	//Create a new header label
+	private JLabel headerLabel = new JLabel("Please choose which key to simulate action \n"
+											+ "to keep your machine awake.");
 	//Create a button object
-	private JButton button1 = new JButton();		
+	private JButton button1 = new JButton("Button!");		
 	//Create a new label object
 	private JLabel label1 = new JLabel("Main Label!");
 	
@@ -28,19 +33,23 @@ public class MainGUI
 	public MainGUI()
 	{
 		contentPanel.setBackground(Color.lightGray);
-		contentPanel.setPreferredSize(new Dimension(200, 200));
+		contentPanel.setPreferredSize(new Dimension(600, 200));
 			
 		mainFrame.setContentPane(contentPanel);
-
 			
 		mainFrame.setVisible(true);
+		
+		contentPanel.add(northPanel, BorderLayout.NORTH);
 
-		contentPanel.add(topLabelPanel);
-		contentPanel.add(bottomButtonPanel);
+		//Add label to header
+		northPanel.add(headerLabel, BorderLayout.NORTH);
 		//Add a label to the Label panel
-		topLabelPanel.add(label1);
+		westPanel.add(label1);
 		//Add a button to the Button panel
-		bottomButtonPanel.add(button1);
+		centerPanel.add(button1);
+		
+		contentPanel.add(westPanel, BorderLayout.WEST);
+		contentPanel.add(centerPanel, BorderLayout.CENTER);
 		
 		mainFrame.pack();
 		
